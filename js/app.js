26ec3b521fc2e08f573c7ad61c7f7347bd9b5e70
jayeen29 <jayeen6783@gmail.com>
2021-08-30 15:34:26 +0600

    error handling done

--- js/app.js
+++ js/app.js
@@ -1,13 +1,48 @@
 const searchFood = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
-    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
-    fetch(url)
-        .then(res => res.json())
-        .then(data => displaySearchResult(data.meals));
+    console.log();
+    if (searchText.replace(/\s/g, '') == '') {
+        searchField.value = '';
+        showError('word');
+        return;
+    }
+    else {
+        const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
+        // clear search
+        searchField.value = '';
+        // Load data
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                //if no result found this condition will  work
+                if (data.meals == null) {
+                    showError('result');
+                }
+                else {
+                    displaySearchResult(data.meals);
+                }
+            });
+    }
 }
+
+//This function will work to show error
+const showError = errorWord => {
+    const searchResult = document.getElementById('search-result');
+    searchResult.innerHTML = '';
+    const error = document.createElement('div');
+    error.classList.add('text-center', 'w-100');
+    error.innerHTML = `
+         <div style="color:red;"><h2>Sorry! No ${errorWord} found</h2></div>
+         `;
+    searchResult.appendChild(error);
+    return;
+}
+
+//This function will work to show search result
 const displaySearchResult = meals => {
     const searchResult = document.getElementById('search-result');
+    searchResult.innerHTML = '';
     meals.forEach(meal => {
         const div = document.createElement('div');
         div.classList.add('col');
@@ -23,6 +58,7 @@ const displaySearchResult = meals => {
         searchResult.appendChild(div);
     })
 }
+//This function will work to show more details of clicked meal
 const loadMealsData = mealId => {
     const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`;
     fetch(url)
